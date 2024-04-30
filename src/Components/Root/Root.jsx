e41e6c57db3b005ab9bc514986fba5640b5e1e4d
ashiqur12315 <ashiqur12315@gmial.com>
2024-04-30 14:40:29 +0530

    Added footer section

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import { Toaster } from "react-hot-toast";
 import { Tooltip } from "react-tooltip";
+import Footer from "../Home/Footer";
 
 
 const Root = () => {
@@ -11,6 +12,7 @@ const Root = () => {
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Toaster></Toaster>
+            <Footer></Footer>
             <Tooltip id="my-tooltip"></Tooltip>
 
         </div>
